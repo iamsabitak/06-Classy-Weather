19e268fdbdc37f87d1d74ba665bbff66a789e9be
sabita <sabitakhadka091@gmail.com>
2023-11-02 15:32:34 +0545

    worked with event handler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class App extends Component {
   render() {
     return (
-      <div>App</div>
-    )
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
