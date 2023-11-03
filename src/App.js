38bd758a16f2d437be9922a95a11cf09c2da97bf
sabita <sabitakhadka091@gmail.com>
2023-11-03 14:48:34 +0545

    learn lifecycle method of class based components

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function formatDay(dateStr) {
 
 export class App extends Component {
   state = {
-    location: "Kathmandu",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
@@ -45,6 +45,8 @@ export class App extends Component {
   // }
   // async fetchWeather(location) {
   fetchWeather = async (location) => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
+
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -69,12 +71,23 @@ export class App extends Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  componentDidMount() {
+    // this.fetchWeather();
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+      localStorage.setItem("loaction", this.state.location);
+    }
+  }
   render() {
     return (
       <div className="app">
@@ -83,7 +96,7 @@ export class App extends Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-        <button onClick={this.fetchWeather}>Get Weather</button>
+        {/* <button onClick={this.fetchWeather}>Get Weather</button> */}
         {this.state.isLoading && <p className="loader">Loading...</p>}
         {this.state.weather.weathercode && (
           <Weather
@@ -113,6 +126,9 @@ class Input extends Component {
   }
 }
 class Weather extends Component {
+  componentWillUnmount() {
+    console.log("unmount");
+  }
   render() {
     const {
       temperature_2m_max: max,
